4df37a4eceb860ca626059d342ac6949ee182342
Ariel Lindemann <ariel.lindemann@gmail.com>
2025-10-28 00:36:45 +0100

    move `write_sequence` to export module

--- app/export.py
+++ app/export.py
@@ -1,11 +1,10 @@
 import json
-from ics import Calendar
 import ics
+from datetime import timedelta
 import logging
 logger = logging.getLogger("uvicorn.app.export")
 
-from app.sequence import write_sequence
-from app.models import CalendarConfig, Event
+from app.models import CalendarConfig, Event, Sequence
 
 def read_config(file_path: str) -> CalendarConfig:
     with open(file_path, "r") as file:
@@ -13,7 +12,7 @@ def read_config(file_path: str) -> CalendarConfig:
     return config
 
 
-def export_to_ical(calendar: Calendar, file_name: str):
+def export_to_ical(calendar: ics.Calendar, file_name: str):
     file_path = f"{file_name}.ics"
 
     with open(file_path, "w") as file:
@@ -25,7 +24,7 @@ def export_to_ical(calendar: Calendar, file_name: str):
 
 
 def from_events(events: list[Event], file_name: str) -> str:
-    calendar = Calendar()
+    calendar = ics.Calendar()
     for event in events:
         cal_event = ics.Event()
         cal_event.name = event.name
@@ -34,10 +33,31 @@ def from_events(events: list[Event], file_name: str) -> str:
         calendar.events.add(cal_event)
     return export_to_ical(calendar, file_name)
 
+def write_sequence(sequence: Sequence, calendar: ics.Calendar) -> ics.Calendar:
+    appointment_names = sequence.appointment_names
+
+    start_date = sequence.start_date
+    end_date = sequence.end_date
+
+    recurrence = timedelta(days=sequence.recurrence_interval_days)
+
+    current_date = start_date
+    index = 0
+
+    while current_date <= end_date:
+        event = ics.Event()
+        event.name = appointment_names[index % len(appointment_names)]
+        event.begin = current_date
+        event.make_all_day()
+        calendar.events.add(event)
+
+        current_date += recurrence
+        index += 1
+
+    return calendar
 
 def from_calendar_config(config: CalendarConfig) -> str:
-
-    calendar = Calendar()
+    calendar = ics.Calendar()
 
     for sequence in config.sequences:
         calendar = write_sequence(sequence, calendar)
